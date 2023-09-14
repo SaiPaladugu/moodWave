64551d2b3fce46c0b76fa0b563dd7d5805245d96
Sai Paladugu <Saichandan03@gmail.com>
2023-09-14 08:02:10 -0400

    changed localhost to vercel

--- src/app/mood-home/mood-home.component.ts
+++ src/app/mood-home/mood-home.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class MoodHomeComponent {
   loginToSpotify() {
     const clientId = '9fede7ed32da4540bfe3ece0f874282c';
-    const redirectUri = encodeURIComponent('http://localhost:4200/callback');
+    const redirectUri = encodeURIComponent('https://mood-wave.vercel.app/callback');
     const scopes = encodeURIComponent('user-top-read playlist-modify-private playlist-modify-public');
     
     const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}&scope=${scopes}`;
